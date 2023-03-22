cb252bb910f6ab9e0bb30e30722987683862c826
root <emmanuellaokafor17@gmail.com>
2023-03-22 02:42:22 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+int main(void)
+{
+    char c = '_';
+    
+    _putchar(c); /* prints the character '_' */
+    c = 'p';
+    _putchar(c); /* prints the character 'p' */
+    c = 'u';
+    _putchar(c); /* prints the character 'u' */
+    c = 't';
+    _putchar(c); /* prints the character 't' */
+    c = 'c';
+    _putchar(c); /* prints the character 'c' */
+    c = 'h';
+    _putchar(c); /* prints the character 'h' */
+    c = 'a';
+    _putchar(c); /* prints the character 'a' */
+    c = 'r';
+    _putchar(c); /* prints the character 'r' */
+    c = '\n';
+    _putchar(c); /* prints a new line character */
+    
+    return (0);
+}
+
